5264aa02ef4690e7f2a123ddc5f6d485424a7c83
unknown <guymdls1508@gmail.com>
2020-11-02 15:00:41 +0200

    fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = 'mongodb://localhost:27017/CoffeeDB';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 
 
